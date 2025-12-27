1c00282a8d9b1d15829ce77eec694774de72cf86
Natan Biley <natan.biley@meltwater.com>
2025-12-27 14:45:20 -0500

    Initial Commit

---
+++ MemoryJournal/MemoryJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryJournalApp.swift
+//  MemoryJournal
+//
+//  Created by Natan Biley on 12/27/25.
+//
+
+import SwiftUI
+
+@main
+struct MemoryJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
